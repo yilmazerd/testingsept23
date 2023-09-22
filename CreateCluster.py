9c2df13874628f7f42e0707f5b1fce220ea42b1f
yilmazerd <yilmazerdem@gmail.com>
2023-09-22 19:46:15 -0400

    Update CreateCluster.py

--- CreateCluster.py
+++ CreateCluster.py
@@ -1,42 +1,39 @@
-import boto3
+import subprocess
 
-def create_aurora_postgres_cluster(client, cluster_id, master_username, master_password):
+def run_command(command):
     try:
-        response = client.create_db_cluster(
-            DBClusterIdentifier=cluster_id,
-            Engine='aurora-postgresql',
-            MasterUsername=master_username,
-            MasterUserPassword=master_password,
-            PubliclyAccessible=True
-        )
-        print("Cluster created successfully:", response)
-    except Exception as e:
-        print("Error creating cluster:", e)
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with error: {e}")
 
-def create_db_instance(client, instance_id, cluster_id):
-    try:
-        response = client.create_db_instance(
-            DBInstanceIdentifier=instance_id,
-            DBClusterIdentifier=cluster_id,
-            Engine='aurora-postgresql',
-            DBInstanceClass='db.r5.large',
-            PubliclyAccessible=True
-        )
-        print("DB instance created successfully:", response)
-    except Exception as e:
-        print("Error creating DB instance:", e)
+def create_aurora_postgres_cluster(cluster_id, master_username, master_password):
+    command = f"""aws rds create-db-cluster \
+                  --db-cluster-identifier {cluster_id} \
+                  --engine aurora-postgresql \
+                  --master-username {master_username} \
+                  --master-user-password {master_password} \
+                  --publicly-accessible"""
+    run_command(command)
+
+def create_db_instance(instance_id, cluster_id):
+    command = f"""aws rds create-db-instance \
+                  --db-instance-identifier {instance_id} \
+                  --db-cluster-identifier {cluster_id} \
+                  --engine aurora-postgresql \
+                  --db-instance-class db.r5.large \
+                  --publicly-accessible"""
+    run_command(command)
 
 def main():
-    client = boto3.client('rds', region_name='your-region')  # e.g., 'us-west-1'
-    
     cluster_identifier = "your-cluster-identifier"
     master_username = "masteruser"
     master_password = "masteruserpassword"
     
     instance_identifier = "your-instance-identifier"
     
-    create_aurora_postgres_cluster(client, cluster_identifier, master_username, master_password)
-    create_db_instance(client, instance_identifier, cluster_identifier)
+    create_aurora_postgres_cluster(cluster_identifier, master_username, master_password)
+    create_db_instance(instance_identifier, cluster_identifier)
 
 if __name__ == "__main__":
     main()
